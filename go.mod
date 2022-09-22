8e7a22ee163259588783f30d8d51ce3d861afb2e
ilyassyaf <ilyas.syafii@gmail.com>
2022-09-22 08:24:16 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ilyassyaf/yeyebackend
+
+go 1.19
---
+++ models/userModel.go
@@ -0,0 +1,18 @@
+package models
+
+import ()
+
+type User struct {
+	ID 
+	First_name
+	Last_name
+	Password
+	Email
+	Wallet
+	Token
+	User_type
+	Refresh_token
+	Created_at
+	Updated_at
+	User_id
+}
