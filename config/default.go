f805f59bb5440701a60f0a688f09c635ede527a9
ilyassyaf <ilyas.syafii@gmail.com>
2022-10-13 10:48:22 +0700

    added base_url

--- config/default.go
+++ config/default.go
@@ -9,6 +9,7 @@ import (
 type Config struct {
 	DBUri                  string        `mapstructure:"MONGODB_LOCAL_URI"`
 	RedisUri               string        `mapstructure:"REDIS_URL"`
+	BaseUrl                string        `mapstructure:"BASE_URL"`
 	Port                   string        `mapstructure:"PORT"`
 	AccessTokenPrivateKey  string        `mapstructure:"ACCESS_TOKEN_PRIVATE_KEY"`
 	AccessTokenPublicKey   string        `mapstructure:"ACCESS_TOKEN_PUBLIC_KEY"`
